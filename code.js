91838603c307efcac622acdc726a0c50d93a5615
Lara Bovens <114385168+LaraBovens5@users.noreply.github.com>
2022-12-05 21:23:25 +0100

    Create code.js
    Adding javascript code from existing file

---
+++ code.js
@@ -0,0 +1,93 @@
+// Date and hour changer
+let now = new Date();
+let dateTime = document.querySelector(".dayTime");
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+dateTime.innerHTML = `${day}, ${hours}:${minutes}`;
+
+// Weather API
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector(".searchBar");
+  let city = document.querySelector(".city");
+  if (searchInput.value) {
+    city.innerHTML = `${searchInput.value}`;
+  } else {
+    city.innerHTML = null;
+    alert("Please type your city");
+  }
+
+  let units = "metric";
+  let apiKey = "07c827efc38dec62f78359db1f1b4e72";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=${units}`;
+
+  function searchTemperature(response) {
+    document.querySelector(".city").innerHTML = response.data.name;
+
+    let country = response.data.sys.country;
+    city.innerHTML = `${searchInput.value}, ${country}`;
+
+    let currentCityTemperature = Math.round(response.data.main.temp);
+    let currentTemperature = document.querySelector(".currentTemp");
+    currentTemperature.innerHTML = `${currentCityTemperature}`;
+
+    let inSky = response.data.weather[0].main;
+    let inTheSky = document.querySelector(".description");
+    inTheSky.innerHTML = inSky;
+
+    let humid = response.data.main.humidity;
+    let currentHumidity = document.querySelector(".humidity");
+    currentHumidity.innerHTML = `Humidity: ${humid}%`;
+
+    let windSpeed = Math.round(response.data.wind.speed);
+    let currentWindSpeed = document.querySelector(".wind");
+    currentWindSpeed.innerHTML = `Wind speed: ${windSpeed} km/hr`;
+
+    let minTemp = Math.round(response.data.main.temp_min);
+    let maxTemp = Math.round(response.data.main.temp_max);
+
+    let currentMinTemp = document.querySelector(".min");
+    let currentMaxTemp = document.querySelector(".max");
+
+    currentMinTemp.innerHTML = `Min: ${minTemp}°C`;
+    currentMaxTemp.innerHTML = `Max: ${maxTemp}°C`;
+  }
+  axios.get(apiUrl).then(searchTemperature);
+}
+
+let form = document.querySelector("#search-bar");
+form.addEventListener("submit", search);
+
+// Geolocation API
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "07c827efc38dec62f78359db1f1b4e72";
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+  axios.get(url).then(showWeather);
+}
+
+function showCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let button = document.querySelector("#currentyCity");
+button.addEventListener("click", showCurrentPosition);
